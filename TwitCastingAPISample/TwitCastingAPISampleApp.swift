c2c8a39b06d102972ee251e6cc3036065270c38c
佐藤真樹 <tosakakun@gmail.com>
2022-06-05 15:48:58 +0900

    Initial Commit

---
+++ TwitCastingAPISample/TwitCastingAPISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitCastingAPISampleApp.swift
+//  TwitCastingAPISample
+//
+//  Created by 佐藤真樹 on 2022/06/05.
+//
+
+import SwiftUI
+
+@main
+struct TwitCastingAPISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
