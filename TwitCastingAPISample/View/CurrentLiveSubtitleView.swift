9c9f101d7d2e86214787c054b5e55bbaeb60e462
佐藤真樹 <tosakakun@gmail.com>
2022-06-06 15:44:03 +0900

    Set or Unset Current Live Subtitle を実装

---
+++ TwitCastingAPISample/View/CurrentLiveSubtitleView.swift
@@ -0,0 +1,59 @@
+//
+//  CurrentLiveSubtitleView.swift
+//  TwitCastingAPIDev
+//
+//  Created by tosakakun on 2022/04/30.
+//
+
+import SwiftUI
+import TwitCastingAPI
+
+struct CurrentLiveSubtitleView: View {
+    
+    @EnvironmentObject var auth: TwitCastingAuth
+    
+    @StateObject var viewModel = CurrentLiveSubtitleViewModel()
+    
+    @State var subtitle = ""
+    
+    var body: some View {
+        VStack {
+            TextField("テロップ", text: $subtitle)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button {
+                Task {
+                    await viewModel.setCurrentLiveSubtitle(token: auth.token, subtitle: subtitle)
+                }
+            } label: {
+                Text("テロップを設定")
+            }
+            .padding()
+
+            Button {
+                Task {
+                    await viewModel.unsetCurrentLiveSubtitle(token: auth.token)
+                }
+            } label: {
+                Text("テロップを解除")
+            }
+            .padding()
+            
+            if let response = viewModel.currentLiveSuttitleResponse {
+                Text(response.movieId)
+                Text(response.subtitle ?? "テロップは解除されました")
+            }
+
+            Spacer()
+            
+        }
+        .navigationTitle("Set or Unset Current Live Subtitle テスト")
+        
+    }
+}
+
+struct CurrentLiveSubtitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentLiveSubtitleView()
+    }
+}
---
+++ TwitCastingAPISample/ViewModel/CurrentLiveSubtitleViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  CurrentLiveSubtitleViewModel.swift
+//  TwitCastingAPIDev
+//
+//  Created by tosakakun on 2022/04/30.
+//
+
+import Foundation
+import TwitCastingAPI
+
+class CurrentLiveSubtitleViewModel: ObservableObject {
+    
+    @Published var currentLiveSuttitleResponse: TCCurrentLiveSubtitleResponse?
+    
+    private let api = TwitCastingAPI()
+    
+    func setCurrentLiveSubtitle(token: String, subtitle: String) async {
+        
+        do {
+            
+            let response = try await api.setCurrentLiveSubtitle(token: token, subtitle: subtitle)
+            
+            DispatchQueue.main.async {
+                self.currentLiveSuttitleResponse = response
+            }
+            
+        } catch let error as TCError {
+            print(error.localizedDescription)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
+    
+    func unsetCurrentLiveSubtitle(token: String) async {
+        
+        do {
+            
+            let response = try await api.unsetCurrentLiveSubtitle(token: token)
+            
+            DispatchQueue.main.async {
+                self.currentLiveSuttitleResponse = response
+            }
+            
+        } catch let error as TCError {
+            print(error.localizedDescription)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    
+}
