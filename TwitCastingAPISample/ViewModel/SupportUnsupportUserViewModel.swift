bb535737d2dbd9d65ecfe0302ff2a2620d25be57
佐藤真樹 <tosakakun@gmail.com>
2022-06-06 16:42:34 +0900

    Support and Unsupport User を実装
    実装しました。

---
+++ TwitCastingAPISample/ViewModel/SupportUnsupportUserViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  SupportUnsupportUserViewModel.swift
+//  TwitCastingAPIDev
+//
+//  Created by tosakakun on 2022/05/06.
+//
+
+import Foundation
+import TwitCastingAPI
+
+class SupportUnsupportUserViewModel: ObservableObject {
+    
+    @Published var supportUserResponse: TCSupportUserResponse?
+    
+    @Published var unsupportUserResponse: TCUnsupportUserResponse?
+    
+    private let api = TwitCastingAPI()
+    
+    func supportUser(token: String, targetUserIds: [String]) async {
+        
+        supportUserResponse = nil
+        unsupportUserResponse = nil
+        
+        do {
+            
+            let response = try await api.supportUser(token: token, targetUserIds: targetUserIds)
+            
+            print(response)
+            
+            DispatchQueue.main.async {
+                self.supportUserResponse = response
+            }
+            
+        } catch let error as TCError {
+            print(error.localizedDescription)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func unsupportUser(token: String, targetUserIds: [String]) async {
+        
+        supportUserResponse = nil
+        unsupportUserResponse = nil
+        
+        do {
+            
+            let response = try await api.unsupportUser(token: token, targetUserIds: targetUserIds)
+            
+            print(response)
+            
+            DispatchQueue.main.async {
+                self.unsupportUserResponse = response
+            }
+            
+        } catch let error as TCError {
+            print(error.localizedDescription)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+}
